970906fd435825669f48f924b4fc41c09e907c68
Jaimesanchezgalvis <70351159+Jaimesanchezgalvis@users.noreply.github.com>
2020-09-01 13:17:42 +0200

    Create imagenes.service.ts

---
+++ src/app/imagenes/imagenes.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagenesService {
+
+  constructor(private HttpClient: HttpClient) { }
+
+  getData () {
+    return this.HttpClient.get('https://rickandmortyapi.com/api/character/');
+  }
+}
