a9c3ec3650d03d019ade0817c28ed4a25b5c33c1
Jaimesanchezgalvis <70351159+Jaimesanchezgalvis@users.noreply.github.com>
2020-09-01 13:17:39 +0200

    Create imagenes.service.spec.ts

---
+++ src/app/imagenes/imagenes.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { ImagenesService } from './imagenes.service';
+
+describe('ImagenesService', () => {
+  let service: ImagenesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ImagenesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
